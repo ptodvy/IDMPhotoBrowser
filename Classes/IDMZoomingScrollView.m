85553620ec8aaa02aaeb499ebe2646eb111877b5
J(장유하) <j.ang@daumkakao.com>
2017-07-18 16:51:08 +0900

    apply screen orientation for progress view

--- Classes/IDMZoomingScrollView.m
+++ Classes/IDMZoomingScrollView.m
@@ -285,6 +285,17 @@
 		_photoImageView.frame = frameToCenter;
     
     _verticalContentRatio = self.frame.size.height / self.frame.size.width;
+    
+    CGRect screenBound = [[UIScreen mainScreen] bounds];
+    CGFloat screenWidth = screenBound.size.width;
+    CGFloat screenHeight = screenBound.size.height;
+    
+    if ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationLandscapeLeft || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationLandscapeRight) {
+        screenWidth = screenBound.size.height;
+        screenHeight = screenBound.size.width;
+    }
+    
+    _progressView.frame = CGRectMake((screenWidth-28.)/2., (screenHeight-28.)/2, 28.0f, 28.0f);
 }
 
 
